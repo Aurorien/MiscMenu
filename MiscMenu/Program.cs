6c2f9b19a7ec8c16b81f0668e2618f10a2f89fef
Anna Eklund <anna.eklund@yahoo.se>
2025-05-26 11:08:33 +0200

    Add project files.

---
+++ MiscMenu/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MiscMenu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------- Misc Menu --------------------");
+        }
+    }
+}
